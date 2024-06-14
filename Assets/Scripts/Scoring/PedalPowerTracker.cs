18d30d8ea57e358176fff73cca7c82e6bbf94ae8
Ash Villaschi <fizix@sidefx.dev>
2024-06-14 23:34:37 +0100

    Add a tracker for pedal power

---
+++ Assets/Scripts/Scoring/PedalPowerTracker.cs
@@ -0,0 +1,67 @@
+﻿using Assets.PlayerScripts;
+using SideFX.Events;
+using SideFX.SceneManagement;
+using SideFX.SceneManagement.Events;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace QWOPCycle.Scoring {
+    public sealed class PedalPowerTracker : MonoBehaviour {
+        [field: SerializeField] public float PedalPower { get; private set; }
+
+        [SerializeField] private float _pedalPowerIncrement = 1f;
+        [SerializeField] private float _maxPedalPower = 10f;
+
+        [SerializeField] private InputReader _input;
+        private EventBinding<SceneReady> _sceneReadyBinding;
+        private bool _gameIsRunning;
+        private PedalState _state = PedalState.None;
+
+        private enum PedalState {
+            None, // Used when the game starts, so the player can start with either pedal
+            Left,
+            Right,
+        }
+
+
+        private void OnEnable() {
+            _input.PedalLeftEvent += OnPedalLeft;
+            _input.PedalRightEvent += OnPedalRight;
+
+            _sceneReadyBinding = new EventBinding<SceneReady>(OnSceneReady);
+            EventBus<SceneReady>.Register(_sceneReadyBinding);
+        }
+
+        private void OnDisable() {
+            _input.PedalLeftEvent -= OnPedalLeft;
+            _input.PedalRightEvent -= OnPedalRight;
+            EventBus<SceneReady>.Deregister(_sceneReadyBinding);
+        }
+
+        private void FixedUpdate() {
+            if (_gameIsRunning && PedalPower > 0f) {
+                PedalPower -= _pedalPowerIncrement * Time.fixedDeltaTime;
+                PedalPower = math.clamp(PedalPower, 0f, _maxPedalPower);
+            }
+        }
+
+        private void OnSceneReady(SceneReady e) {
+            _gameIsRunning = e.Scene is GameplayScene;
+        }
+
+
+        private void OnPedalLeft() {
+            if (_gameIsRunning && _state is not PedalState.Left) {
+                _state = PedalState.Left;
+                PedalPower += 1;
+            }
+        }
+
+        private void OnPedalRight() {
+            if (_gameIsRunning && _state is not PedalState.Right) {
+                _state = PedalState.Right;
+                PedalPower += 1;
+            }
+        }
+    }
+}
