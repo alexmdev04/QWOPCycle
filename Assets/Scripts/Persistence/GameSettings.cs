206e4c46639cedee0a2b14cfb4323e6fd4cfbc78
Ash Villaschi <fizix@sidefx.dev>
2024-06-15 00:23:07 +0100

    Added simple game settings and save data

---
+++ Assets/Scripts/Persistence/GameSettings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace QWOPCycle.Persistence {
+    [Serializable]
+    public struct AudioSettings {
+        public bool IsMusicOn;
+        public bool IsSfxOn;
+        [Range(0f, 1f)] public float MusicVolume;
+    }
+
+    [Serializable]
+    public sealed class GameSettings {
+        private const string FILENAME = "settings.data";
+        private static readonly string FILEPATH = Path.Combine(Application.persistentDataPath, FILENAME);
+
+
+        [field: SerializeField] public AudioSettings Audio { get; private set; }
+
+
+        public static GameSettings Load() {
+            if (!File.Exists(FILEPATH)) {
+                var data = new GameSettings();
+                data.Save();
+                return data;
+            }
+
+            string json = File.ReadAllText(FILEPATH);
+            return JsonUtility.FromJson<GameSettings>(json);
+        }
+
+        public void Save() {
+            string json = JsonUtility.ToJson(this);
+            File.WriteAllText(FILEPATH, json);
+        }
+    }
+}
---
+++ Assets/Scripts/Persistence/SaveData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using QWOPCycle.Scoring;
+using UnityEngine;
+
+namespace QWOPCycle.Persistence {
+    [Serializable]
+    public sealed class SaveData {
+        private const string FILENAME = "save.data";
+        private static readonly string FILEPATH = Path.Combine(Application.persistentDataPath, FILENAME);
+
+        public uint HighScore { get; private set; }
+
+        public static SaveData Load() {
+            if (!File.Exists(FILEPATH)) {
+                var data = new SaveData();
+                data.Save();
+                return data;
+            }
+
+            string json = File.ReadAllText(FILEPATH);
+            return JsonUtility.FromJson<SaveData>(json);
+        }
+
+
+        public void Save() {
+            string json = JsonUtility.ToJson(this);
+            File.WriteAllText(FILEPATH, json);
+        }
+
+        public bool PostScore(ScoreTracker score) {
+            if (score.Score > HighScore) {
+                HighScore = score.Score;
+                Save();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
