3b99b29bc9d16bac382ebd2a957b1b31e9cbf7af
SantiagoApontePerez <Lamleecs@outlook.com>
2024-06-14 16:35:39 +0100

    Update QWOPCharacter.cs

--- Assets/Scripts/Player/QWOPCharacter.cs
+++ Assets/Scripts/Player/QWOPCharacter.cs
@@ -6,10 +6,20 @@ using UnityEngine;
 public class QWOPCharacter : MonoBehaviour
 {
     public InputReader inputReader;
+
+    private Rigidbody rigidBody;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        SetupPlayer();
+    }
+
+    private void SetupPlayer()
+    {
+        if (rigidBody is null)
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
     }
 
     // Update is called once per frame
