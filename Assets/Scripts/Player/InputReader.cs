b4bf181fd017b7a617c46520e397b4b8147c83e0
SantiagoApontePerez <Lamleecs@outlook.com>
2024-06-14 16:19:37 +0100

    Added delegates to an input reader script.

--- Assets/Scripts/Player/InputReader.cs
+++ Assets/Scripts/Player/InputReader.cs
@@ -1,18 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class InputReader : MonoBehaviour
+namespace Assets.PlayerScripts
 {
-    // Start is called before the first frame update
-    void Start()
+    [CreateAssetMenu(fileName = "Input Reader", menuName = "InputReader/Inputs")]
+    public class InputReader : ScriptableObject, Controls.IPlayActions
     {
-        
-    }
+        #region InputReader Setup
+        private Controls _controls;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private void OnEnable()
+        {
+            InitController();
+            _controls.Play.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _controls.Play.Disable();
+        }
+
+        private void InitController()
+        {
+            _controls = (_controls is null) ? new Controls() : _controls;
+            _controls.Play.SetCallbacks(this);
+        }
+
+        #endregion
+
+        #region IPlayerControllerActions
+
+        public event Action BalanceRightEvent = delegate { };
+        public event Action BalanceLeftEvent = delegate { };
+        public event Action PedalLeftEvent = delegate { };
+        public event Action PedalRightEvent = delegate { };
+
+        public void OnBalanceRight(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                BalanceRightEvent?.Invoke();
+            }
+        }
+
+        public void OnBalanceLeft(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                BalanceLeftEvent?.Invoke();
+            }
+        }
+
+        public void OnPedalLeft(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                PedalLeftEvent?.Invoke();
+            }
+        }
+
+        public void OnPedalRight(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                PedalRightEvent?.Invoke();
+            }
+        }
+        #endregion
         
     }
 }
