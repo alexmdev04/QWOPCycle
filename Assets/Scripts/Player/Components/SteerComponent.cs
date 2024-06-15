23ff3d2cd9acb11b6744bcf626e5491ca2a3bc84
Alex M <alexmdev04@gmail.com>
2024-06-15 18:41:27 +0100

    Player Components no longer get QWOPCharacter component

--- Assets/Scripts/Player/Components/SteerComponent.cs
+++ Assets/Scripts/Player/Components/SteerComponent.cs
@@ -1,58 +1,49 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using SideFX.Anchors;
+using QWOPCycle.Player;
 using SideFX.Events;
 using SideFX.SceneManagement.Events;
-using Unity.IO.LowLevel.Unsafe;
-using Unity.Logging;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace QWOPCycle.Gameplay {
     public class SteerComponent : MonoBehaviour {
-        private QWOPCharacter _character;
         public float steeringMultiplier = 0.001f;
-        public bool _canMove = true;
-        [SerializeField] private GameManagerAnchor _gameManagerAnchor;
-
-        private void Awake() {
-            _character = GetComponent<QWOPCharacter>();
-        }
+        public bool canMove = true;
+        private QWOPCharacter _character;
+        private EventBinding<PlayerFellOver> _playerFellOverBinding;
 
         private EventBinding<SceneReady> _sceneReadyBinding;
 
+        private float PlayerXDelta => _character.BalanceComponent.TiltAngle * Time.deltaTime * steeringMultiplier;
+
+        private void Update() {
+            if (!canMove) return;
+            transform.position = transform.position.With(transform.position.x - PlayerXDelta);
+        }
+
         private void OnEnable() {
             _sceneReadyBinding = new EventBinding<SceneReady>(OnSceneReady);
+            _playerFellOverBinding = new EventBinding<PlayerFellOver>(OnFellOver);
             EventBus<SceneReady>.Register(_sceneReadyBinding);
         }
 
         private void OnDisable() {
             EventBus<SceneReady>.Deregister(_sceneReadyBinding);
+            EventBus<PlayerFellOver>.Deregister(_playerFellOverBinding);
+        }
+
+        public void SetCharacter(QWOPCharacter character) {
+            _character = character;
         }
 
         private void OnSceneReady(SceneReady e) {
-            _canMove = true;
+            canMove = true;
             _character.RigidBody.constraints = (RigidbodyConstraints)48 + 10; // freeze rot x and y, freeze pos x and z
-            _character.BalanceComponent.FellOverEvent += DisableMove;
         }
 
-        private void DisableMove() {
-            _canMove = false;
-            _character.RigidBody.constraints = (RigidbodyConstraints) 48 + 8; // freeze rot x and y, freeze pos z
+        private void OnFellOver() {
+            canMove = false;
+            _character.RigidBody.constraints = (RigidbodyConstraints)48 + 8; // freeze rot x and y, freeze pos z
             //_character.RigidBody.velocity = _character.RigidBody.velocity.With(x: -PlayerXDelta);
         }
-
-        private void Update() {
-            if (!_canMove) { return; }
-            transform.position = transform.position.With(
-                x: Mathf.Clamp(
-                    transform.position.x - PlayerXDelta,
-                    (-_gameManagerAnchor.Value.blockWidth / 2) + _character.bikeWidth,
-                    (_gameManagerAnchor.Value.blockWidth / 2) - _character.bikeWidth
-                )
-            );
-        }
-
-        private float PlayerXDelta => _character.BalanceComponent.TiltAngle * Time.deltaTime * steeringMultiplier;
     }
 }
