9a4672a5b748d2ef03f6704ba062fe5bcf5474ec
Ash Villaschi <fizix@sidefx.dev>
2024-06-15 22:32:09 +0100

    Update distance travelled label

--- Assets/Scripts/Interface/HUDController.cs
+++ Assets/Scripts/Interface/HUDController.cs
@@ -1,21 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+using QWOPCycle.Scoring;
+using Unity.Logging;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-namespace QWOPCycle
-{
-    public class HUDController : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
+namespace QWOPCycle.Interface {
+    [RequireComponent(typeof(UIDocument))]
+    public sealed class HUDController : MonoBehaviour {
+        private UIDocument _doc;
+        private Label _currentDistanceLabel;
+        private Label _bestDistanceLabel;
+        private Label _scoreLabel;
+
+        [SerializeField] private ScoreTracker _scoreTracker;
+
+        private string CurrentScore => _scoreTracker.Score.ToString("#.#");
+
+        private void Awake() {
+            _doc = GetComponent<UIDocument>();
+            _currentDistanceLabel = _doc.rootVisualElement.Q<Label>("current-distance");
+            _bestDistanceLabel = _doc.rootVisualElement.Q<Label>("best-distance");
+            _scoreLabel = _doc.rootVisualElement.Q<Label>("score");
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
+        private void Start() {
+            if (_scoreTracker == null) {
+                Log.Error("HUDController: ScoreTracker is null");
+                enabled = false;
+            }
+        }
+
+        private void LateUpdate() {
+            _currentDistanceLabel.text = $"{_scoreTracker.DistanceTravelled:N1}m";
+            // _bestDistanceLabel.text = _scoreTracker.BestDistance.ToString();
+            _scoreLabel.text = _scoreTracker.Score.ToString();
+            // _doc.rootVisualElement.MarkDirtyRepaint();
         }
     }
 }
