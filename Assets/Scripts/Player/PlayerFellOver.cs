2b787f67f34c16d81ebabbf062614b387d905aac
Ash Villaschi <fizix@sidefx.dev>
2024-06-15 18:01:23 +0100

    add player fell over event

---
+++ Assets/Scripts/Player/PlayerFellOver.cs
@@ -0,0 +1,7 @@
+﻿using SideFX.Events;
+
+namespace QWOPCycle.Player {
+    // Notify all game systems that the player fell over
+    // Usage: EventBus<PlayerFellOver>.Raise(default);
+    public readonly struct PlayerFellOver : IEvent { }
+}
