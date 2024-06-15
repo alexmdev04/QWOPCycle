53007949df115785fb3b2a5d89521bfcb3277d8e
Ash Villaschi <fizix@sidefx.dev>
2024-06-15 23:35:35 +0100

    Try to save score on game over

--- Assets/Scripts/Persistence/SaveDataManager.cs
+++ Assets/Scripts/Persistence/SaveDataManager.cs
@@ -1,3 +1,4 @@
+using QWOPCycle.Gameplay;
 using SideFX.Events;
 using Unity.Logging;
 using UnityEngine;
@@ -20,6 +21,7 @@ namespace QWOPCycle.Persistence {
 
         private EventBinding<SaveGameEvent> _saveGameBinding;
         private EventBinding<SaveSettingsEvent> _saveSettingsBinding;
+        private EventBinding<GameOverEvent> _gameOverBinding;
 
         private void Awake() {
             if (Instance == null) Instance = this;
@@ -32,14 +34,17 @@ namespace QWOPCycle.Persistence {
 
             _saveGameBinding = new EventBinding<SaveGameEvent>(OnSaveGame);
             _saveSettingsBinding = new EventBinding<SaveSettingsEvent>(OnSaveSettings);
+            _gameOverBinding = new EventBinding<GameOverEvent>(OnGameOver);
 
             EventBus<SaveGameEvent>.Register(_saveGameBinding);
             EventBus<SaveSettingsEvent>.Register(_saveSettingsBinding);
+            EventBus<GameOverEvent>.Register(_gameOverBinding);
         }
 
         private void OnDisable() {
             EventBus<SaveGameEvent>.Deregister(_saveGameBinding);
             EventBus<SaveSettingsEvent>.Deregister(_saveSettingsBinding);
+            EventBus<GameOverEvent>.Deregister(_gameOverBinding);
         }
 
         private void OnSaveGame() {
@@ -50,6 +55,12 @@ namespace QWOPCycle.Persistence {
         private void OnSaveSettings() {
             Log.Debug("SaveDataManager : Saving Settings");
             Settings.Save();
+        }
+
+
+        private void OnGameOver(GameOverEvent e) {
+            Save.PostScore(e.Score);
+            Save.PostDistance(e.Distance);
         }
     }
 }
