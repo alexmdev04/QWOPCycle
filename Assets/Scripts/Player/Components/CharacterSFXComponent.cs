fad7220310cc2c24318328e4cf8ef31e482f530a
SantiagoApontePerez <Lamleecs@outlook.com>
2024-06-16 16:51:13 +0100

    Added SFX to characterSFXComponent.

--- Assets/Scripts/Player/Components/CharacterSFXComponent.cs
+++ Assets/Scripts/Player/Components/CharacterSFXComponent.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using QWOPCycle.Player;
+using SideFX.Events;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace QWOPCycle
 {
@@ -14,33 +19,45 @@ namespace QWOPCycle
         #endregion
         #region Vars
         [Header("Audio Clips")]
-        public AudioClip balanceSound;
-        public AudioClip pedalSound;
-        public AudioClip fallDownSound;
-        public AudioClip smackSound;
+        public AudioClip[] balanceSound;
+        public AudioClip[] pedalSound;
+        public AudioClip[] fallDownSound;
+        public AudioClip[] smackSound;
         public bool CanPlaySfx { get; set; }
         #endregion
+        #region Event Bus
+        private EventBinding<PlayerFellOver> _playerFellOverBinding;
+        #endregion
+#region Intialisation
+        private void Start() {
+            BindEventsToAudio();
+        }
+        private void BindEventsToAudio() {
+            _playerFellOverBinding = new EventBinding<PlayerFellOver>(PlayFallDownSfx);
+            EventBus<PlayerFellOver>.Register(_playerFellOverBinding);
+        }
+#endregion
         #region Audio Logic
         public void PlayBalanceSfx() {
             if (!CanPlaySfx) return;
             if (balanceAudioSource != null
                 && balanceSound != null) {
-                balanceAudioSource.clip = balanceSound;
+                balanceAudioSource.clip = ChooseRandomClip(balanceSound);
                 balanceAudioSource.Play();
             } else {Debug.LogWarning("Character SFX Component : balance audio source or sound not set.");}
         }
         public void PlayPedalSfx() {
             if (!CanPlaySfx) return;
             if (pedalAudioSource != null && pedalSound != null) {
-                pedalAudioSource.clip = pedalSound;
+                pedalAudioSource.clip = ChooseRandomClip(pedalSound);
                 pedalAudioSource.Play();
             } else {Debug.LogError("Character SFX Component : Pedal audio source or sound not set.");}
         }
-        public void PlayFallDownSfx() {
+        private void PlayFallDownSfx() {
             if (!CanPlaySfx) return;
             if (fallDownAudioSource != null
                 && fallDownSound != null) {
-                fallDownAudioSource.clip = fallDownSound;
+                fallDownAudioSource.clip = ChooseRandomClip(fallDownSound);
                 fallDownAudioSource.Play();
             } else {Debug.LogError("Character SFX Component : Fall down audio source or sound not set.");}
         }
@@ -48,9 +65,19 @@ namespace QWOPCycle
             if (!CanPlaySfx) return;
             if (smackAudioSource != null
                 && smackSound != null) {
-                smackAudioSource.clip = smackSound;
+                smackAudioSource.clip = ChooseRandomClip(smackSound);
                 smackAudioSource.Play();
             } else {Debug.LogError("Character SFX Component : Smack audio source or sound not set.");}
+        }
+
+        private AudioClip ChooseRandomClip(AudioClip[] clipArray) {
+            if (clipArray == null
+                || clipArray.Length == 0) {
+                Debug.LogError($"Character SFX Component : The clip array {clipArray} is null or empty.");
+                return null;
+            }
+            int randomIndex = Random.Range(0, clipArray.Length);
+            return clipArray[randomIndex];
         }
         #endregion
     }
