7f4d5afa65d105ab15543b9b1be9550c64d09357
Alex M <alexmdev04@gmail.com>
2024-06-16 23:06:33 +0100

    Add Pedal Power Bar

--- Assets/Scripts/Interface/HUDController.cs
+++ Assets/Scripts/Interface/HUDController.cs
@@ -23,7 +23,7 @@ namespace QWOPCycle.Interface {
         private float _warningLerpValue;
         private bool _warningLerpUp;
         private float _levelIncreaseLabelTimer;
-        private VisualElement _pedalPowerBar;
+        private ProgressBar _pedalPowerBar;
 
         private EventBinding<LevelIncreaseEvent> _levelIncreaseBinding;
 
@@ -45,7 +45,7 @@ namespace QWOPCycle.Interface {
             _warningLabel = _doc.rootVisualElement.Q<Label>("warning");
             _runTimeLabel = _doc.rootVisualElement.Q<Label>("run-time");
             _levelIncreaseLabel = _doc.rootVisualElement.Q<Label>("level-increase");
-            _pedalPowerBar = _doc.rootVisualElement.Q<VisualElement>("pedal-power");
+            _pedalPowerBar = _doc.rootVisualElement.Q<ProgressBar>("pedal-power");
         }
 
         private void Start() {
@@ -81,11 +81,7 @@ namespace QWOPCycle.Interface {
             float bestDistance = Mathf.Max(SaveDataManager.Instance.Save.BestDistance, _scoreTracker.DistanceTravelled);
             _bestDistanceLabel.text = $"Best Distance: {bestDistance:N1}m";
             _runTimeLabel.text = $@"{_scoreTracker.RunTime:mm\:ss}";
-            _pedalPowerBar.layout.Set(
-                _pedalPowerBar.layout.position.x,
-                _pedalPowerBar.layout.position.y,
-                Mathf.Lerp(0f, 416f, _pedalTracker.PedalPowerRatio),
-                _pedalPowerBar.layout.height);
+            _pedalPowerBar.value = _pedalTracker.PedalPowerRatio;
 
             WarningLabelTick();
             LevelIncreaseLabelTick();
