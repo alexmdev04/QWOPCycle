67c78bd96450b8642f41cbf12802aaea4caf43c0
Alex M <alexmdev04@gmail.com>
2024-06-16 17:55:53 +0100

    Remove LevelIncrease

--- Assets/Scripts/Player/LevelIncrease.cs
+++
@@ -1,7 +0,0 @@
-using SideFX.Events;
-
-namespace QWOPCycle.Player {
-    // Notify all game systems that the level has increased
-    // Usage: EventBus<PlayerFellOver>.Raise(default);
-    public readonly struct LevelIncrease : IEvent { }
-}
